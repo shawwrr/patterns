7cffa1a28ad4b79b9b1388ead1dbbe864746efb3
shawwrr <shhaworkk@gmail.com>
2026-03-02 10:46:42 +0530

    Add files via upload
    alphabets pattern

---
+++ ch.java
@@ -0,0 +1,19 @@
+public class ch {
+    
+    public static void main(String[] args) {
+        int i,j;
+        char ch='A';
+        for(i=1;i<=4;i++)
+        {
+            for(j=1;j<=i;j++)
+            {
+                System.out.print(ch);
+                ch++;
+
+            }
+            System.out.println();
+        }
+    }
+}
+
+
