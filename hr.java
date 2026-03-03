f93a4222a003e5525ff3adfd7398505af788a965
shawwrr <shhaworkk@gmail.com>
2026-03-03 15:55:30 +0530

    Add files via upload
    hollow rectangle pattern

---
+++ hr.java
@@ -0,0 +1,28 @@
+public class hr
+{
+    public static void hollowrect(int r, int c)
+    {
+        for(int i=1;i<=r;i++)
+        {
+            for(int j=1;j<=c;j++)
+            {
+                if(i==1||i==r||j==1||j==c)
+                {
+                    System.out.print("*");
+
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+
+    public static void main(String[] args) 
+    {
+        hollowrect(4,5);
+        
+    }
+}
